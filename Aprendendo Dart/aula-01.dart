9d27c0d27f2539eff9bb71ed4acc9174a4cba032
Nathan Duarte Aguilar <nathanduarteaguilar@gmail.com>
2024-03-15 00:33:48 -0300

    update

--- Aprendendo Dart/aula-01.dart
+++ Aprendendo Dart/aula-01.dart
@@ -106,7 +106,44 @@ void main(){
   print(e.toUpperCase());
   
 
-// .toLowerCase() deixa todos os caracteres de uma String minusculos
+  // .toLowerCase() deixa todos os caracteres de uma String minusculos
   print(f.toLowerCase());
+  
+  print("/*********************************************************************************************************************/");
+ 
+  print("/************************************ listas, conjutos e mapas ******************************************************/"); 
+
+  // as listas podem ser dos tipos, String, int, double, var e const
+  List lista = [1, "Dois", 3.0];
+
+  //vamos ver o tamanho da lista, retorna um valor inteiro 
+  print(lista.length);
+  
+  // retorna o valor de cada posição dentro da lista
+  print(lista[0]);
+  print(lista[1]);
+  print(lista[2]);
+
+  //adicionando valor dentro da lista
+  lista.add("Quatro");
+  lista.add(5);
+  lista.add(6.0);
+
+  print(lista);
+  
+  // Vamos criar uma lista com os valores inteiros e tentando adicionar outro valores 
+  // para declarar o tipo da lista a expressao é um pouco diferente em dart
+  List<int> numeros = [1, 2, 3];
+  print(numeros);
+
+  //vamos tentar colocar uma string dentro da lista
+  //numeros.add("Olá Mundo");   // Apague as primeiras "//" para visualizar o erro
+
+  //vamos tentar colocar um Double dentro da lista
+  //numeros.add(6.0);   // Apague as primeiras "//" para visualizar o erro
+
+  //vamos tentar colocar uma Bool dentro da lista
+  //numeros.add(true);   // Apague as primeiras "//" para visualizar o erro
+
   
 }
